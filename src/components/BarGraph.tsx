75b1017e00b394624090932267472d382be1cb1b
Agustin-Cuello <cuello.130678@alu.uns.edu.ar>
2026-02-10 18:40:57 -0300

    Starting base for grafana plugin

---
+++ src/components/BarGraph.tsx
@@ -0,0 +1,44 @@
+import React, { useRef, useEffect } from 'react';
+
+interface Props {
+  values: number[];
+  width: number;
+  height: number;
+}
+
+
+
+export function BarCanvas({ values, width, height, onClick }: Props & { onClick?: (event: React.MouseEvent<HTMLCanvasElement>) => void }) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      return; 
+    }
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) {
+      return;
+    }
+
+    ctx.clearRect(0, 0, width, height);
+
+    const max = Math.max(...values, 1);
+    const barWidth = width / values.length;
+
+    values.forEach((v, i) => {
+      const barHeight = (v / max) * height;
+
+      ctx.fillStyle = '#5794F2';
+      ctx.fillRect(
+        i * barWidth,
+        height - barHeight,
+        barWidth - 2,
+        barHeight
+      );
+    });
+  }, [values, width, height]);
+
+  return <canvas ref={canvasRef} width={width} height={height} />;
+}
---
+++ src/components/SimplePanel.tsx
@@ -0,0 +1,109 @@
+import React, { useRef } from 'react';
+import { PanelProps } from '@grafana/data';
+import { SimpleOptions } from 'types';
+import { css, cx } from '@emotion/css';
+import { FieldType } from '@grafana/data';
+import { BarCanvas } from './BarGraph';
+
+interface Props extends PanelProps<SimpleOptions> {}
+
+const getStyles = () => {
+  return {
+    wrapper: css`
+      font-family: Open Sans;
+      position: relative;
+    `,
+    svg: css`
+      position: absolute;
+      top: 0;
+      left: 0;
+    `,
+    textBox: css`
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      padding: 10px;
+    `,
+  };
+};
+
+  //Click handler para realizar un zoom a la barra clickeada
+const handleClick = (event: React.MouseEvent<HTMLCanvasElement>) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvas = canvasRef.current;
+  if (!canvas) return;
+
+  const rect = canvas.getBoundingClientRect();
+
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+};
+
+export const SimplePanel: React.FC<Props> = ({ data, width, height }) => {
+  const series = data.series[0];
+  const valueField = series?.fields.find(f => f.type === FieldType.number);
+
+  if (!valueField) {
+    return <div>No data</div>;
+  }
+
+  const values = [];
+  for (let i = 0; i < valueField.values.length; i++) {
+    const v = valueField.values.get(i);
+    if (typeof v === 'number') {
+      values.push(v);
+    }
+  }
+
+  return (
+    <BarCanvas
+      values={values}
+      width={width}
+      height={height}
+      onClick={handleClick}
+    />
+  );
+
+};
+
+/*
+export const SimplePanel: React.FC<Props> = ({ options, data, width, height, fieldConfig, id }) => {
+  const theme = useTheme2();
+  const styles = useStyles2(getStyles);
+
+  if (data.series.length === 0) {
+    return <PanelDataErrorView fieldConfig={fieldConfig} panelId={id} data={data} needsStringField />;
+  }
+
+  return (
+    <div
+      className={cx(
+        styles.wrapper,
+        css`
+          width: ${width}px;
+          height: ${height}px;
+        `
+      )}
+    >
+      <svg
+        className={styles.svg}
+        width={width}
+        height={height}
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
+      >
+        <g>
+          <circle data-testid="simple-panel-circle" style={{ fill: theme.colors.primary.main }} r={100} />
+        </g>
+      </svg>
+
+      <div className={styles.textBox}>
+        {options.showSeriesCount && (
+          <div data-testid="simple-panel-series-counter">Number of series: {data.series.length}</div>
+        )}
+        <div>Text option value: {options.text}</div>
+      </div>
+    </div>
+  );
+};*/
