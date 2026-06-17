38ce5a2eb8c42efb8ad5cf2d4724126e42fa7171
Agustin-Cuello <agcuello3@gmail.com>
2026-06-17 14:55:09 -0300

    Arreglada paleta de colores

--- src/components/colorscales.ts
+++ src/components/colorscales.ts
@@ -1,36 +1,5 @@
 export const exampleScales = [
     {
-        name: 'PuOr',
-        scale: [
-            '#2D004B',
-            '#542788',
-            '#8073AC',
-            '#B2ABD2',
-            '#D8DAEB',
-            '#F7F7F7',
-            '#FEE0B6',
-            '#FDB863',
-            '#E08214',
-            '#B35806',
-            '#7F3B08'
-        ]
-    },
-    {
-        name: 'RdBu',
-        scale: [
-            '#67001F',
-            '#B2182B',
-            '#D6604D',
-            '#F4A582',
-            '#FDDBC7',
-            '#F7F7F7',
-            '#D1E5F0',
-            '#92C5DE',
-            '#4393C3',
-            '#2166AC',
-            '#053061'
-        ]
-    },
     {
         name: 'greys',
         scale: [
@@ -42,76 +11,6 @@ export const exampleScales = [
             '#B3B3B3',
             '#D9D9D9',
             '#FFFFFF'
-        ]
-    },
-    {
-        name: 'greens',
-        scale: [            
-            '#002B12',
-            '#005A22',
-            '#0A8F38',
-            '#1CC54E',
-            '#53E373',
-            '#8FF0A3',
-            '#C8F9D2',
-            '#F2FFF5'
-        ]    
-    },
-    {
-        name: 'blues',
-        scale: [
-            '#001A4D',
-            '#003380',
-            '#004DB3',
-            '#0066E6',
-            '#3399FF',
-            '#80C1FF',
-            '#B3DCFF',
-            '#EAF5FF'
-        ]
-    },
-    {
-        name: 'spectral',
-        scale: [
-            '#9E0142',
-            '#D53E4F',
-            '#F46D43',
-            '#FDAE61',
-            '#FEE08B',
-            '#FFFFBF',
-            '#E6F598',
-            '#ABDDA4',
-            '#66C2A5',
-            '#3288BD',
-            '#5E4FA2'
-        ]
-    },
-    {
-        name: 'turbo',
-        scale: [
-            '#30123B',
-            '#4145AB',
-            '#2A9FD6',
-            '#1FD3A4',
-            '#A3F12E',
-            '#F9E721',
-            '#F9B618',
-            '#F56A1C',
-            '#D52A2A',
-            '#7A0403'
-        ]
-    },
-    {
-        name: 'vivid',
-        scale: [
-            '#E41A1C',
-            '#377EB8',
-            '#4DAF4A',
-            '#984EA3',
-            '#FF7F00',
-            '#FFFF33',
-            '#A65628',
-            '#F781BF'
         ]
     }
 ];
